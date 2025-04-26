6bf64471b79f6d86a1ab4021fbeb93075302d4ad
ThomasMjake <tranhoangphu9a13@gmail.com>
2025-04-26 17:51:32 +0700

    Update Finish  WebWithLRF

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const formOpenBtn = document.querySelector("#form-open"),
     home = document.querySelector(".home"),
-    formContainer = document.querySelector(".form-container"),
+    formContainer = document.querySelector(".form_container"),
     formCloseBtn = document.querySelector(".form_close"),
     signupBtn = document.querySelector("#signup"),
     loginBtn = document.querySelector("#login"),
@@ -8,3 +8,25 @@ const formOpenBtn = document.querySelector("#form-open"),
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        let getPwInput= icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+        } else {
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash");
+        }
+    });
+});
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
